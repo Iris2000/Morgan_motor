fb1329678b47b979acf98907b1ef56383ca4028a
Galaxysternmond <gonniesternmond@gmail.com>
2019-11-23 22:23:35 +0800

    Search box

--- first_page.php
+++ first_page.php
@@ -3,23 +3,23 @@
   <style>
     .navi {
       background-color:#001A0D;
-
+      text-align: center;
+      color: white;
+      font-size: 30px;
     }
     .center{
       text-align: center;
     }
+    
   </style>
   <head>
     <title>Morgan Motor</title>
   </head>
   <body>
-
     <form action="search.php" method="get">
       <div class='navi'>
-        <br>
-        <img src="images/morgan_logo.png" alt="Morgan_Motor_Logo" style="text-align: left; height:50px;"><div class='center'><input type="text" name="search"> <input type="submit" value="Go"></div>
-        <br>
-        <br>
+        <img src="images/morgan_logo.png" alt="Morgan_Motor_Logo" style="text-align: left; height:100px;">
+        <div class='center'><input type="text" name="search" style="border-radius: 5px; width: 300px;">&nbsp<input type="submit" value="Go" style="border-radius: 5px;"></div>
       </div>
   </form>
   </body>
