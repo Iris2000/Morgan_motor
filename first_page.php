2a99574fef063f5cb7c60a7c8db47326187f750d
Galaxysternmond <gonniesternmond@gmail.com>
2019-11-23 10:06:28 +0800

    add first page and search page

---
+++ first_page.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <style>
+    .navi {
+      background-color:#001A0D;
+      align:"center";
+    }
+  </style>
+  <head>
+    <title>Morgan Motor</title>
+  </head>
+  <body>
+
+    <form action="search.php" method="get">
+      <div class='navi'>
+        <input type="text" name="search"> <input type="submit" value="Go">
+      </div>
+  </form>
+  </body>
+</html>
+</body>
+</html>
